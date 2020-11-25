2d0cd2a755cad6254ebdd2fa35237b7484a92d1f
Afformativ <40630611+Afformativ@users.noreply.github.com>
2020-11-25 20:14:24 +0200

    Add files via upload

---
+++ SolidEx1/SolidEx1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidEx1
+{
+    //Який принцип S.O.L.I.D. порушено? Виправте!
+    // Single responsibility principle
+    class Item
+    {
+
+    }
+    class Order
+    {
+        private List<Item> _itemList;
+
+        internal List<Item> ItemList
+        {
+            get { return _itemList; }
+            set { _itemList = value; }
+        }
+        public void GetItems() {/*...*/}
+        public void AddItem(Item item) {/*...*/}
+        public void DeleteItem(Item item) {/*...*/}
+
+    }
+
+    class OrderCalculations
+    {
+        public void CalculateTotalSum(Order order) {/*...*/}
+        public void GetItemCount(Order order) {/*...*/}
+    }
+
+    class OrderRepository
+    {
+        public void Load(int orderId) {/*...*/}
+        public void Save(Order order) {/*...*/}
+        public void Update(Order order) {/*...*/}
+        public void Delete(Order order) {/*...*/}
+    }
+
+    class PrintManager
+    {
+        public void PrintOrder(Order order) {/*...*/}
+        public void ShowOrder(Order order) {/*...*/}
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ SolidEx3/SolidEx3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SolidEx3
+{
+    //квадрат наслідується від прямокутника!!!
+    // Порушено Liskov substitution principle. Ми не можемо використовувати квадрат там, де
+    // використовуємо прямокутник (замінити клас базовим), адже метод обчислення площі
+    // видає нелогічний з точки зору користувача результат.
+
+    // Щоб вирішити дану проблему, можна визначити інтерфейс IShape
+
+    interface IShape
+    {
+        int GetRectangleArea();
+    }
+
+    class Rectangle : IShape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int GetRectangleArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Square : IShape
+    {
+        public int Side { get; set; }
+
+        public int GetRectangleArea()
+        {
+            return Side * Side;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.Side = 5;
+
+            Console.WriteLine(square.GetRectangleArea());
+            Console.ReadKey();
+        }
+    }
+}
